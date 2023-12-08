4725dd8310caaa1f078531be5bc225a82e0416ed
lucas <lucasgabriellima@gmail.com>
2023-12-08 14:54:53 -0300

    Fix ERR_HTTP_HEADERS_SENT error when user is not authorized

--- backend/middleware/validateTokenHandler.ts
+++ backend/middleware/validateTokenHandler.ts
@@ -1,22 +1,24 @@
 import asyncHandler from 'express-async-handler';
 import * as jwt from 'jsonwebtoken';
 
-const validateToken = asyncHandler(async (req, res, next) => {
+const validateToken = asyncHandler(async (req: any, res, next) => {
     let token;
     let authHeader: string = (req.headers.Authorization! || req.headers.authorization) as string;
     if (authHeader && authHeader.startsWith('Bearer')) {
         token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, _decoded) => {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, decoded) => {
             if (err) {
-                return res.status(401).json({message: 'User is not authorized'});
+                res.status(401).json({message: 'User is not authorized'});
+                throw new Error('User is not authorized')
             }
+            req.user = decoded
+            next();
         })
-
-        return next();
     }
 
     if (!token) {
         res.status(401).json({message: 'User is not authorized or bearer token is missing'});
+        throw new Error('User is not authorized or bearer token is missing')
     }
 })
 
